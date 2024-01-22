2a7e5748c2cf0b6c3afaf5d2c99181fc87964e8a
Bilal Afzal <106112000+bilalxelp@users.noreply.github.com>
2024-01-22 19:21:57 +0530

    Update demo_app.py

--- demo_app.py
+++ demo_app.py
@@ -24,7 +24,7 @@ def main():
     st.title("Audio Analysis App")
 
     # Load dataframe from Excel file (replace with your actual file path)
-    csv_path = "demo_output.csv"
+    csv_path = "new_demo.csv"
     df = pd.read_csv(csv_path, index_col=0)
 
 
